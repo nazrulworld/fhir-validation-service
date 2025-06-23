b932f1ef613ef25d4026324b980c6fc6bb1cebdb
Md Nazrul Islam <connect2nazrul@gmail.com>
2025-06-24 00:45:36 +0200

    :sparkles: roolback db init script

--- docker/postgres-init/init-database.sh
+++ docker/postgres-init/init-database.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "${PG_USER:-postgres}" --dbname "postgres" <<EOSQL
-ALTER USER postgres WITH PASSWORD '${PG_PASSWORD}';
-DO \$\$
-BEGIN
-   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = '${PG_DATABASE}') THEN
-      CREATE DATABASE ${PG_DATABASE};
-   END IF;
-END
-\$\$;
+# Set password for postgres user
+psql -v ON_ERROR_STOP=1 --username "$PG_USER" --dbname "postgres" <<-EOSQL
+    ALTER USER postgres WITH PASSWORD '$PG_PASSWORD';
+    SELECT 'CREATE DATABASE $PG_DATABASE'
+    WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$PG_DATABASE')\gexec
 EOSQL
